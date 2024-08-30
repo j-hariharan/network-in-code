b8d529053e14dec025650fae92f018135d0411f2
vignarajpai@outlook.com <vignarajpai@outlook.com>
2024-08-30 21:34:34 +0530

    some left out

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -3,6 +3,7 @@
 import { motion } from 'framer-motion';
 import styles from '../styles';
 import { fadeIn } from '../utils/motion';
+import { basePath } from '../next.config';
 
 const ExploreCard = ({ id, imgUrl, title, description, index, active, handleClick }) => (
   <motion.div
@@ -12,7 +13,7 @@ const ExploreCard = ({ id, imgUrl, title, description, index, active, handleClic
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
-    <img src={imgUrl} alt={title} className="absolute w-full h-full object-cover rounded-[24px]" />
+    <img src={basePath + imgUrl} alt={title} className="absolute w-full h-full object-cover rounded-[24px]" />
 
     { active !== id ? (
       <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
@@ -21,7 +22,7 @@ const ExploreCard = ({ id, imgUrl, title, description, index, active, handleClic
     ) : (
       <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
         <div className={`${styles.flexCenter} w-[60px] h-[60px] glassmorphism mb-[16px] rounded-[12px]`}>
-          <img src="/headset.svg" alt="headset" className="w-1/2 h-1/2 object-cotain" />
+          <img src={`${basePath}/headset.svg`} alt="headset" className="w-1/2 h-1/2 object-cotain" />
         </div>
         {/* <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">Enter Metaverse</p> */}
         <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
